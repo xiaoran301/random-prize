055b87abde4bbd5c1459bd5bc471a6506539276d
xuhengjin <xuhengjin@bytedance.com>
2021-04-28 18:44:38 +0800

    ™|~_~| init

---
+++ js/member.js
@@ -0,0 +1,118 @@
+var member = [
+  {
+    "phone": 1,
+    "name": "李正卿"
+  },
+  {
+    "phone": 2,
+    "name": "程君洋"
+  },
+  {
+    "phone": 3,
+    "name": "陈雷"
+  },
+  {
+    "phone": 4,
+    "name": "杜逸"
+  },
+  {
+    "phone": 5,
+    "name": "崔银杰"
+  },
+  {
+    "phone": 6,
+    "name": "古珺和"
+  },
+  {
+    "phone": 7,
+    "name": "大厨"
+  },
+  {
+    "phone": 8,
+    "name": "杨帆"
+  },
+  {
+    "phone": 9,
+    "name": "郎轩"
+  },
+  {
+    "phone": 10,
+    "name": "向光源"
+  },
+  {
+    "phone": 11,
+    "name": "孟祥睿"
+  },
+  {
+    "phone": 12,
+    "name": "邱柱擎"
+  },
+  {
+    "phone": 13,
+    "name": "许恒金"
+  },
+  {
+    "phone": 14,
+    "name": "王同"
+  },
+  {
+    "phone": 15,
+    "name": "黄鑫"
+  },
+  {
+    "phone": 16,
+    "name": "陈玲玲"
+  },
+  {
+    "phone": 17,
+    "name": "叶晨"
+  },
+  {
+    "phone": 18,
+    "name": "龚新民"
+  },
+  {
+    "phone": 19,
+    "name": "李晨旭"
+  },
+  {
+    "phone": 20,
+    "name": "路文"
+  },
+  {
+    "phone": 21,
+    "name": "王振新"
+  },
+  {
+    "phone": 22,
+    "name": "王欢"
+  },
+  {
+    "phone": 23,
+    "name": "高强"
+  },
+  {
+    "phone": 24,
+    "name": "张文彬"
+  },
+  {
+    "phone": 25,
+    "name": "高枫"
+  },
+  {
+    "phone": 26,
+    "name": "邢增"
+  },
+  {
+    "phone": 27,
+    "name": "李志鹏"
+  },
+  {
+    "phone": 28,
+    "name": "任志勇"
+  },
+  {
+    "phone": 29,
+    "name": "张庆杰"
+  }
+]
